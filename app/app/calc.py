619a3f675adf299873d58c111195c3c56017eb1d
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-08-08 14:24:10 +0300

    setup postgres db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculate functions..
+"""
+
+def add(x, y):
+  """adds x and y and return result."""
+  return x + y
+
+
+def subtract(x, y):
+  """subtract x from y and return result."""
+  return y - x
+  
